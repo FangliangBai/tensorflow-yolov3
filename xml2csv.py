2e3b9049d03c86462717d10e5cb2ba0a047973f2
devbox <fangliangbai@gmail.com>
2019-07-01 17:26:08 +0100

    tiny changes

--- xml2csv.py
+++ xml2csv.py
@@ -32,11 +32,11 @@ xxx/xxx.jpg 18.19,6.32,424.13,421.83,20 323.86,2.65,640.0,421.94,20
 xxx/xxx.jpg 48,240,195,371,11 8,12,352,498,14
 # image_path x_min, y_min, x_max, y_max, class_id  x_min, y_min ,..., class_id
 """
-import os
 import glob
-import pandas as pd
-import numpy as np
 import xml.etree.ElementTree as ET
+
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 XML_PATH = '/media/kent/DISK2/SBRI_Project/dataset_mmwave/file to train_yolo/Annotation/'
@@ -62,13 +62,13 @@ def xml_to_csv(path):
         for member in root.findall('object'):
             if len(member) == 5:
                 value = (
-                    # int(root.find('size')[0].text),
-                    # int(root.find('size')[1].text),
-                    round(float(member[4][0].text)),
-                    round(float(member[4][1].text)),
-                    round(float(member[4][2].text)),
-                    round(float(member[4][3].text)),
-                    class_name2index(member[0].text, NAMES),
+                        # int(root.find('size')[0].text),
+                        # int(root.find('size')[1].text),
+                        round(float(member[4][0].text)),
+                        round(float(member[4][1].text)),
+                        round(float(member[4][2].text)),
+                        round(float(member[4][3].text)),
+                        class_name2index(member[0].text, NAMES),
                 )
                 # The classes for mm-wave dataset is
                 # 0 scissors, 1 knife, 2 water, 3 oil, 4 cardboard.
@@ -83,7 +83,7 @@ def xml_to_csv(path):
         xml_list.append(line)
         # print(cont)
         cont += 1
-        
+
     xml_list = np.array(xml_list)
     xml_df = pd.DataFrame(xml_list)
 
@@ -95,7 +95,7 @@ def xml_to_csv(path):
 
     xml_train_df.to_csv((OUTPUT_PATH + OUTPUT_TRAIN_TBLNAME), index=False, header=False, sep=' ', )
     xml_test_df.to_csv((OUTPUT_PATH + OUTPUT_TEST_TBLNAME), index=False, header=False, sep=' ', )
-    
+
 
 def class_name2index(name, names):
     """
@@ -127,7 +127,7 @@ def refine_txt(file):
     s = s.replace(', ', ',')
     s = s.replace(',_,', ' ')
     s = s.replace(',_', '')
-    
+
     f = open(file, 'w')
     f.write(s)
     f.close()
