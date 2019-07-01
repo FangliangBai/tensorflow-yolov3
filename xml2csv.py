a0ef85079ace8062ae5b064326a3b95bf6384718
devbox <fangliangbai@gmail.com>
2019-07-01 21:07:06 +0100

    tiny changes

--- xml2csv.py
+++ xml2csv.py
@@ -34,7 +34,9 @@ xxx/xxx.jpg 48,240,195,371,11 8,12,352,498,14
 """
 import glob
 import xml.etree.ElementTree as ET
+import logging
 
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -47,14 +49,18 @@ OUTPUT_TRAIN_TBLNAME = 'mm-wave_train.txt'
 OUTPUT_TEST_TBLNAME = 'mm-wave_test.txt'
 
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
+logger = logging.getLogger()
+
+
 def xml_to_csv(path):
     xml_list = []
     cont = 1
-    for xml_file in glob.glob(path + '/*.xml'):
+    for xml_file in tqdm(glob.glob(path + '/*.xml')):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         if root.findall('object') == []:
-            print("[Warning] {} contains no <object> section.".format(xml_file))
+            logger.warning('{} contains no <object> section.'.format(xml_file))
             continue
         line = []
         img_path = [str(IMG_PATH + root.find('filename').text)]
@@ -70,19 +76,18 @@ def xml_to_csv(path):
                         round(float(member[4][3].text)),
                         class_name2index(member[0].text, NAMES),
                 )
-                # The classes for mm-wave dataset is
-                # 0 scissors, 1 knife, 2 water, 3 oil, 4 cardboard.
-                # However, cardboard is not considered, so we skip it in the following if block.
-                if value[-1] == 4:
+                if value[-1] == -1:
                     continue
                 value = np.array(value, dtype=int).tolist()
                 line += [value, '_']
             else:
-                print("[Warning] {} contains incomplete <object> section.".format(xml_file))
+                logger.warning('{} contains incomplete <object> section.'.format(xml_file))
                 continue
         xml_list.append(line)
         # print(cont)
         cont += 1
+
+    logger.info('List generation complete')
 
     xml_list = np.array(xml_list)
     xml_df = pd.DataFrame(xml_list)
@@ -100,16 +105,19 @@ def xml_to_csv(path):
 def class_name2index(name, names):
     """
     convert class name to corresponding index number.
-    :param name:
-    :param names:
-    :return:
+    :param name  : str, string extracted from XML file
+    :param names : str, path to class.names
+    :return      : int, index of correspoding class name
     """
     name_list = pd.read_csv(names, header=None, index_col=None, dtype='str')
     name_list = name_list.values
     name_list = np.reshape(name_list, [-1])
-    print('[Error] The xml class name', name, 'can not match the pre-defined names.')
-    index = np.where(name_list == name)
-    return index[0][0]
+    try:
+        index = np.where(name_list == name)
+        return index[0][0]
+    except:
+        logger.warning('The xml class name {} can not match any of pre-defined names. It will be skipped.'.format(name))
+        return -1
 
 
 def refine_txt(file):
@@ -135,10 +143,11 @@ def refine_txt(file):
 
 def main():
     xml_to_csv(XML_PATH)
-    print('Successfully converted xml to csv.')
+    logger.info('Successfully converted XML to CSV.')
 
 
 if __name__ == '__main__':
     main()
     refine_txt((OUTPUT_PATH + OUTPUT_TRAIN_TBLNAME))
     refine_txt((OUTPUT_PATH + OUTPUT_TEST_TBLNAME))
+    logger.info('CSV table refined.')
